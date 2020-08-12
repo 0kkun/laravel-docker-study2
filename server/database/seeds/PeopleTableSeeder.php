211792b08f015b23a3d1f30079bc97ee7de60776
0kkun <shinji.okumoto.01@gmail.com>
2020-08-13 08:47:56 +0900

    peopeleテーブル用のseeder作成

---
+++ server/database/seeds/PeopleTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PeopleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('people')->insert([
+            [
+                'name' => '鈴木太郎',
+                'age' => '26',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+             ],
+            [
+                'name' => '小玉 隆博',
+                'age' => '26',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => '岩井 圭',
+                'age' => '26',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+        ]);
+    }
+}
